baa71b70da3eb1c6c1d250f9354dcaa08c36b212
Prasanthipandrada <111628437+Prasanthipandrada@users.noreply.github.com>
2022-10-07 16:44:55 +0530

    n=int(input()) l=list(map(int, input().split())) k=len(l) s1=[] s2=[] for i in l:     if i in range(1,(k//2)+1):         s1.append(i)     else:         s2.append(i) print(sum(s1)) print(sum(s2))

---
+++ sum_of_first_half_and_second_half_in_an_array.py
@@ -0,0 +1,12 @@
+n=int(input())
+l=list(map(int, input().split()))
+k=len(l)
+s1=[]
+s2=[]
+for i in l:
+    if i in range(1,(k//2)+1):
+        s1.append(i)
+    else:
+        s2.append(i)
+print(sum(s1))
+print(sum(s2))
